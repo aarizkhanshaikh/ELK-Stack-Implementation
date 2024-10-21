2de43dfbb91eb3b5e2ea9f68a82ce8d87e528736
Aariz Khan Sajidahmed Shaikh <aariz92shaikh@gmail.com>
2024-10-22 01:35:46 +0530

    Add files via upload

---
+++ start_elk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo systemctl start elasticsearch
+sudo systemctl start logstash
+sudo systemctl start kibana
---
+++ stop_elk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo systemctl stop elasticsearch
+sudo systemctl stop logstash
+sudo systemctl stop kibana
